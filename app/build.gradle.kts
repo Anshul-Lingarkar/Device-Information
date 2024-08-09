6a7e0bf915fd441c06e3ebdecc49f5adc50598e6
Anshul_Lingarkar <anshul.lingarkar@gmail.com>
2024-08-09 11:26:42 -0700

    APP-03 : Final file formatting changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,31 +53,27 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
+    implementation("com.google.dagger:dagger-android:2.16")
+    kapt("com.google.dagger:dagger-compiler:2.16")
+    implementation("javax.inject:javax.inject:1")
 
+    /*
+    Testing Dependencies - JUnit, Mockito, Coroutines Testing, AndroidX Testing
+    */
 
-    // JUnit
     testImplementation("junit:junit:4.13.2")
 
-    // Mockito
     testImplementation("org.mockito:mockito-core:4.3.1")
     testImplementation("org.mockito:mockito-inline:4.3.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
 
-    // Coroutines Testing
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
 
-    // AndroidX Testing
     testImplementation("androidx.test.ext:junit:1.1.3")
-    //testImplementation("androidx.test.core:1.4.0")
     testImplementation("androidx.arch.core:core-testing:2.1.0")
 
-    // Kotlin Standard Library
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
-    // Robolectric
     testImplementation("org.robolectric:robolectric:4.9.2")
-    implementation("com.google.dagger:dagger-android:2.16")
-    kapt("com.google.dagger:dagger-compiler:2.16")
-    implementation("javax.inject:javax.inject:1")
     testImplementation("androidx.test:core:1.5.0")
     testImplementation("androidx.test:rules:1.5.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
